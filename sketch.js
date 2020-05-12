8ce3ef1a65a6ebec1a1912146bea072fe3d8a2ad
Surabhi Patel <whatinthesamhill2006@gmail.com>
2020-05-12 13:49:17 -0400

    constrain added

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const c = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -33,8 +34,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
+    Clog = new Log(230,180,80,PI/2);
 
     bird = new Bird(100,100);
+
+    Chain = new chain(bird.body,Clog.body);
 
 }
 
@@ -61,4 +65,9 @@ function draw(){
 
     bird.display();
     platform.display();
+
+    Clog.display();
+    
+    Chain.display();
+   
 }
